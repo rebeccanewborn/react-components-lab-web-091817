b2951a05e84b97cb4e5b3b99db203247d8b1a3db
rebeccanewborn <rjn58@cornell.edu>
2017-11-19 11:56:21 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,22 +1,62 @@
-// Make sure to import React and ReactDOM
+import React from "react";
+import ReactDOM from "react-dom";
 
-const BUTCHER_PRODUCTS = [
-  'Tenderloin',
-  'Short ribs',
-  'Beef shin',
-  'Ribeye'
-];
+const BUTCHER_PRODUCTS = ["Tenderloin", "Short ribs", "Beef shin", "Ribeye"];
 
 // Define these exported classes
-export class OlderCoaster extends React.Component {};
-export class InFrontOfYou extends React.Component {};
-export class ButcherShop extends React.Component {};
+export class OlderCoaster extends React.Component {
+  render() {
+    return React.createElement("div", { className: "oldercoaster" }, [
+      React.createElement(
+        "p",
+        {},
+        "Two grannies having the time of their life!"
+      ),
+      React.createElement("p", {}, "Passengers:"),
+      React.createElement("ul", {}, [
+        React.createElement("li", {}, "Agnes"),
+        React.createElement("li", {}, "Muriel")
+      ])
+    ]);
+  }
+}
+export class InFrontOfYou extends React.Component {
+  render() {
+    return React.createElement("div", {}, [
+      React.createElement("p", {}, "You shouldn't look too far."),
+      React.createElement(
+        "p",
+        {},
+        "Sometimes, the solution is right in front of you."
+      )
+    ]);
+  }
+}
+
+export class ButcherShop extends React.Component {
+  render() {
+    return React.createElement("div", { className: "butcher-shop" }, [
+      React.createElement(
+        "p",
+        {},
+        "Hello! We have the following products for sale today:"
+      ),
+      React.createElement(
+        "ul",
+        {},
+        BUTCHER_PRODUCTS.map(elt => {
+          return React.createElement("li", {}, `${elt}`);
+        })
+      )
+    ]);
+  }
+}
 
 ReactDOM.render(
-  React.createElement('div', {}, [
+  React.createElement("div", {}, [
     React.createElement(OlderCoaster),
     React.createElement(InFrontOfYou),
     React.createElement(ButcherShop)
   ]),
-  document.getElementById('global')
+  document.getElementById("global")
 );
